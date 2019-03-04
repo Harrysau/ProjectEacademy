f3be7c172b87f34b5a2006eee5f561120862d106
Harrysau <harrysauchhinsun@ymail.com>
2019-03-04 22:26:26 +0700

    Final Commit From me

--- ProjectEacademy/Controllers/UserController.cs
+++ ProjectEacademy/Controllers/UserController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using ProjectEacademy.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +10,11 @@ namespace ProjectEacademy.Controllers
 {
     public class UserController : Controller
     {
+        private ApplicationDbContext _context;
+        public UserController()
+        {
+            _context = new ApplicationDbContext();
+        }
         // GET: User
         public ActionResult Index()
         {
@@ -21,6 +28,40 @@ namespace ProjectEacademy.Controllers
         public ActionResult UserProfile()
         {
             return View();
+        }
+
+        //[Route("User/EditProfile/{UserID}")]
+        public ActionResult EditProfile(string UserID)
+        {
+            UserEdit models = new UserEdit();
+            var userquery = _context.Users.Single(u => u.Id == UserID);
+            models.Username = userquery.User;
+            models.UID = userquery.Id;
+            models.SchoolName = userquery.SchoolName;
+            models.FullName = userquery.FullName;
+            return View(models);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult EditProfile (UserEdit models)
+        {
+            var userquery = _context.Users.Single(u => u.Id == models.UID);
+
+            try
+            {
+                userquery.FullName = models.FullName;
+                userquery.SchoolName = models.SchoolName;
+                userquery.User = models.Username;
+
+                _context.SaveChanges();
+            }
+            catch(Exception)
+            {
+                ModelState.AddModelError("EditError", "Cant Edit this model");
+                return View(models);
+            }
+            return RedirectToAction("UserProfile");
         }
     }
 }
---
+++ ProjectEacademy/Models/EditUserModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectEacademy.Models
+{
+    public class EditUserModels
+    {
+    }
+    public class UserEdit
+    {
+        public string UID { get; set; }
+        public string FullName { get; set; }
+        public string SchoolName { get; set; }
+        public string Username { get; set; }
+    }
+}
